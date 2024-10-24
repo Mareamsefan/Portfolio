739ef628c9d2e2b180419cd29729e23216a51fb1
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-10-24 10:50:46 +0200

    comment.

--- api/backend/src/features/projects/service/indext.ts
+++ api/backend/src/features/projects/service/indext.ts
@@ -1,6 +1,7 @@
 import { mapProject } from "../mappers";
 import { getAllProjects } from "../repository";
 
+// her skal du kun sende videre data til rep, kanskje kan du validere dersom du ikke gjør det i controller
 export const getProjects = async () => {
     const projects = await getAllProjects();
     return projects.map(mapProject);
